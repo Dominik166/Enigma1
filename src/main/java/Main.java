49c9b27ec7d87b830f49059c635f74a0ea63d870
Dominik Staniszewski <dymo.s@interia.pl>
2020-02-29 10:27:26 +0100

    Task 6 refactored and back fixed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,15 @@
-import impl.Cipher;
-import impl.CesarCipher;
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.awt.*;
-
-public class TestMain extends Application {
-    String resourcePath = "/fxml/Test.fxml";
+public class Main extends Application {
+    String resourcePath = "/fxml/Main.fxml";
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/Test.fxml"));
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource(resourcePath));
         Parent root = loader.load();
         Scene scene = new Scene(root, 600, 600);
         stage.setTitle("Enigma1");
